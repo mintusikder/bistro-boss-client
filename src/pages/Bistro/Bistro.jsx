70633ba6859d392b46d47562a0bdca6db91ab3cd
Mintu Sikder <mintusikder15@gmail.com>
2024-09-16 14:19:27 +0600

    added bistro service

---
+++ src/pages/Bistro/Bistro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import service from "../../../public/assets/home/chef-service.jpg";
+
+const Bistro = () => {
+  return (
+    <div className="relative ">
+      <img src={service} alt="Chef Service" />
+      <div className="absolute top-1/2 w-3/4 h-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-white  p-10 flex flex-col items-center justify-center">
+        <h3 className="text-2xl font-bold">Bistro Boss</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Bistro;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Category from "../../Category/Category";
+import Bistro from "../../Bistro/Bistro";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Category></Category>
+      <Bistro></Bistro>
     </div>
   );
 };
