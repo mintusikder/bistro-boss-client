902bbbe1ca050ca4e41c1fd6eff0f24d099731e5
Mintu Sikder <mintusikder15@gmail.com>
2024-09-16 12:08:18 +0600

    update route and added footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import React from "react";
+import "./index.css";
+import { router } from "./routes/Routers";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="max-w-screen-xl	 mx-auto">
+    <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>
+);
+
