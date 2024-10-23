eac85921e4c0f641d4fe7d9cde951fabb1679275
Mintu Sikder <mintusikder15@gmail.com>
2024-10-23 18:19:32 +0600

    added useMenu

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,10 +5,14 @@ import Bistro from "../../Bistro/Bistro";
 import PopularManue from "../../PopularManue/PopularManue";
 import Featured from "../../Featured/Featured";
 import Testimonials from "../../Testimonials/Testimonials";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Bistro Boss | Home</title>
+      </Helmet>
       <Banner></Banner>
       <Category></Category>
       <Bistro></Bistro>
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from '../../../../public/assets/menu/banner3.jpg';
 
 const Menu = () => {
-    return (
-        <div>
-            <h2>menu</h2>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+   
+    </div>
+  );
 };
 
 export default Menu;
+//fdf
+//fefef
